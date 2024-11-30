26458c492f351278055648ca25e68e6883cf0df5
alchymista79 <tokarevartem000@gmail.com>
2024-11-30 15:02:41 +0300

    Add files via upload

---
+++ module_4/module_4_1/Module_4_1.py
@@ -0,0 +1,8 @@
+from Module_4.module_4_1.fake_math import divide as fm  # fm - от англ. false method
+from true_math import divide as tm  # tm - от англ. true method
+
+
+print(fm(50, 5))
+print(fm(10, 0))
+print(tm(49, 7))
+print(tm(45, 0))
