874ab9f7040cc25f82ed6645558fa1f9996f10a3
alchymista79 <tokarevartem000@gmail.com>
2024-12-22 18:30:55 +0300

    22.12.24

--- Module_6/module_6_1.py
+++ Module_6/module_6_1.py
@@ -12,19 +12,25 @@ class Animal:
             print(f'{self.name} не стал есть {food.name}')
             self.alive = False
 
+
 class Plant:
     edible = False
+
     def __init__(self, name):
         self.name = name
+
 
 class Mammal(Animal):
     pass
 
+
 class Predator(Animal):
     pass
 
+
 class Flower(Plant):
     pass
+
 
 class Fruit(Plant):
     edible = True
@@ -37,6 +43,7 @@ p2 = Fruit('Заводной апельсин')
 
 print(a1.name)
 print(p1.name)
+
 print(a1.alive)
 print(a2.fed)
 a1.eat(p1)
